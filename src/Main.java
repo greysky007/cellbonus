4dae0f2a1330a5865e9a7c35f22f0915578126e0
greysky <antonysnork@gmail.com>
2022-06-13 13:06:03 +0300

    Reformating code

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
         int payment = 1100;//сумма пополнения
         int x = 1000;//необходимая сумма пополнения для получения бонусов
         int z = 100;//сумма соответсвующая 1 бонусу
-        int bonus = (payment/z);
+        int bonus = (payment / z);
         int result;
 
-        if (payment >= x){
-            result = account+payment+bonus;
-            System.out.println("Начислено бонусов: "+(bonus));
+        if (payment >= x) {
+            result = account + payment + bonus;
+            System.out.println("Начислено бонусов: " + (bonus));
         } else {
             result = account + payment;
             System.out.println("Начислено бонусов: 0");

f680642aa79b82552d37d498ceae8be480f91f48
greysky <antonysnork@gmail.com>
2022-06-18 16:16:27 +0300

    Changed names of variables

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int account = 100;//начальная сумма счета
         int payment = 1100;//сумма пополнения
-        int x = 1000;//необходимая сумма пополнения для получения бонусов
-        int z = 100;//сумма соответсвующая 1 бонусу
-        int bonus = (payment / z);
+        int minPay = 1000;//необходимая сумма пополнения для получения бонусов
+        int sumBonus = 100;//сумма соответсвующая 1 бонусу
+        int bonus = (payment / sumBonus);
         int result;
 
-        if (payment >= x) {
+        if (payment >= minPay) {
             result = account + payment + bonus;
             System.out.println("Начислено бонусов: " + (bonus));
         } else {
